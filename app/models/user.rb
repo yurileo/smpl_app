94a317e6ce880972a4b25c30016778bf722b9f19
Yury <yurileo@yandex.ru>
2019-04-10 14:14:43 +0000

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+    #self.table_name = 'dbo.users'
+    
     before_save {self.email = email.downcase}
     validates :name, presence: true, length: {maximum: 50 }
     validates :password, length: {minimum: 6 }
